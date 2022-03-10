b969019f2fabe01b02b767ad2c8ee7ead597d429
Edgardo Gustavo Vergara - UTN <62915181+GustavoVergara87@users.noreply.github.com>
2022-03-10 16:46:59 -0300

    RawMaterialsForm Validations

--- src/components/RawMaterialsForm.js
+++ src/components/RawMaterialsForm.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { Field, Form } from 'react-final-form';
 
+const required = (value) => (value ? undefined : 'Required');
+
+const mustBeNumber = (value) =>
+	isNaN(value) ? 'Must be a number' : undefined;
+
+const composeValidators =
+	(...validators) =>
+	(value) =>
+		validators.reduce(
+			(error, validator) => error || validator(value),
+			undefined
+		);
+
 function RawMaterialsForm(props) {
 	const { id, description, price, amount, unit } = props.rawMaterial;
 
@@ -13,6 +26,7 @@ function RawMaterialsForm(props) {
 			<div>
 				<label> {label}</label>
 				<input {...input} autoComplete='off' />
+				{meta.error && meta.touched && <span>{meta.error}</span>}
 			</div>
 		);
 	};
@@ -29,10 +43,26 @@ function RawMaterialsForm(props) {
 							name='description'
 							label='Description'
 							component={renderInput}
+							validate={required}
 						/>
-						<Field name='price' label='Price' component={renderInput} />
-						<Field name='amount' label='Amount' component={renderInput} />
-						<Field name='unit' label='Unit' component={renderInput} />
+						<Field
+							name='price'
+							label='Price'
+							component={renderInput}
+							validate={composeValidators(required, mustBeNumber)}
+						/>
+						<Field
+							name='amount'
+							label='Amount'
+							component={renderInput}
+							validate={composeValidators(required, mustBeNumber)}
+						/>
+						<Field
+							name='unit'
+							label='Unit'
+							component={renderInput}
+							validate={required}
+						/>
 
 						<div style={{ display: 'flex', flexFlow: 'row-reverse' }}>
 							<button>Save</button>
